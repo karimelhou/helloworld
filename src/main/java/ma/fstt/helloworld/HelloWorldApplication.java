9dd34ded9bac8c420ce0351503ced5db7851a532
Karim ElHoumaini <elhoumaini1@gmail.com>
2024-01-05 00:29:26 +0100

    Update HelloWorldApplication.java

--- src/main/java/ma/fstt/helloworld/HelloWorldApplication.java
+++ src/main/java/ma/fstt/helloworld/HelloWorldApplication.java
@@ -16,7 +16,7 @@ public class HelloWorldApplication {
     }
     @GetMapping("/hello")
     public String helloWorld() {
-        return "Hello, my World!";
+        return "Hello, mmy World!";
     }
 
     @GetMapping("/hello/{name}")
